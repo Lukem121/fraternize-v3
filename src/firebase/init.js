11f823e4a69248ce9bee651be2127e3f3fbbbeb6
luke askew <lukeask@hotmail.co.uk>
2020-11-16 00:59:20 +0000

    🦞Added depolyment

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
-// The configuration below is not sensitive data. You can serenely add your config here
 const config = {
   apiKey: 'AIzaSyDb_QyaAapepiJ0ZVkZfijaWOzhUakUiEw',
   authDomain: 'frat-bento.firebaseapp.com',
